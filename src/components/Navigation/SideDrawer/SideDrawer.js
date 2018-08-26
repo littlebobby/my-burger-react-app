f6aaac6868336a99a448fa8cb3cb7d5b978d1a2c
Xiaoyun <zxiaoyun98@gmail.com>
2018-08-26 19:30:59 +0200

    backdrop for sidebar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,16 +2,27 @@ import React from 'react'
 import classes from './SideDrawer.css'
 import Logo from '../../UI/Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
-const sideDrawer = () => (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+const sideDrawer = (props) => {
+  let assignedClasses = [classes.SideDrawer, classes.Close]
+  if (props.open) {
+    assignedClasses = [classes.SideDrawer, classes.Open]
+  }
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={assignedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
-)
+    </Aux>
+  )
+}
 
 export default sideDrawer;
