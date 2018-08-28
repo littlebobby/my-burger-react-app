209ea974c5e73083f508799d7d5263be1060813e
Xiaoyun <zxiaoyun98@gmail.com>
2018-08-28 18:24:41 +0200

    changing folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux'
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredient)
-    .map(key => <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredient[key]}</li>)
+class OrderSummary extends Component {
+  // * this can be a functional component
+  componentWillUpdate() {
+    console.log('will update')
+  }
+  render() {
+  const ingredientSummary = Object.keys(this.props.ingredient)
+    .map(key => <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredient[key]}</li>)
   
-  console.log(ingredientSummary)
-  
+  // console.log(ingredientSummary)
   return (
     <Aux>
       <h3>Your Order</h3>
@@ -15,12 +19,14 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
-      <p><strong>Total Price: {props.totalPrice.toFixed(2)}</strong></p>
+      <p><strong>Total Price: {this.props.totalPrice.toFixed(2)}</strong></p>
       <p>Continue to checkout?</p>
-      <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+      <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
     </Aux>
   )
 
+  }
 }
-export default orderSummary;
+
+export default OrderSummary;
